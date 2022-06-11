2242132e0c718ff1274780d59ee3d0b783f2ccf7
fikadulem <fikadulem@gmail.com>
2022-06-10 22:22:59 -0700

    updated

--- workspace/extract.py
+++ workspace/extract.py
@@ -18,19 +18,28 @@ import json
 from models import NearEarthObject, CloseApproach
 
 
-def load_neos(filename="C://Training//Udacity//project//workspace//data//neos.csv"):
+def load_neos(filename="/Users/fikadu.balcha/Training/near_earth_objects/workspace/data/neos.csv"):
     """Read near-Earth object information from a CSV file.
 
     :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
     # TODO: Load NEO data from the given CSV file.
-    neos = {}
+    neos = []
     with open(filename) as f:
         reader = csv.reader(f)
+        header = next(reader)
         for line in reader:
+            neos.append(line)
+        #lens = len(neos)
+        #print(lens)
+        print(f"There are {len(neos)} rows in this file")
+        print(header)
+        #print(neos)
+        #filename.close()
    # TO DO: add
-    return neos
+    #return neos
+load_neos()
 
 
 def load_approaches(filename="C://Training//Udacity//project//workspace//data//cad.json"):
@@ -43,6 +52,8 @@ def load_approaches(filename="C://Training//Udacity//project//workspace//data//c
         reader = csv.reader(f)
         for line in reader:
 
-    return approaches
+            return approaches
+
+    approaches
     # TODO: Load close approach data from the given JSON file.
     # return ()
